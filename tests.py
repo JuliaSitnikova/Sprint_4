1bdf09c342b80717903a32f011090e80964bf48a
Julia Sitnikova <sitnikova23@yandex.ru>
2023-12-13 22:49:25 +0300

    First commit in Sprint_4

--- tests.py
+++ tests.py
@@ -22,3 +22,63 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book(self):
+        collector = BooksCollector()
+        collector.collector.add_new_book('Крик')
+        assert len(collector.get_books_raiting()) == 1
+
+    def test_set_book_genre(collector):
+        collector = BooksCollector()
+        collector.set_book_genre('Крик', 'Ужасы')
+        assert collector.get_book_genre('Крик') == 'Ужасы'
+
+    def test_get_book_genre(collector):
+        collector = BooksCollector()
+        collector.set_book_genre('Крик', 'Ужасы')
+        assert collector.get_book_genre('Крик') == 'Ужасы'
+
+    def test_get_books_with_specific_genre(collector):
+        collector = BooksCollector()
+        collector.set_book_genre('Отель', 'Детективы')
+        collector.set_book_genre('Пуаро', 'Детективы')
+        collector.set_book_genre('Черновик', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Детективы') == ['Отель', 'Пуаро']
+
+    def test_get_books_genre(collector):
+        collector = BooksCollector()
+        collector.set_book_genre('Приключения Буратино', 'Мультфильмы')
+        assert collector.get_books_genre() == {'Приключения Буратино': 'Мультфильмы'}
+
+    def test_get_books_for_children(collector):
+        collector = BooksCollector()
+        collector.set_book_genre('Черновик', 'Фантастика')
+        collector.set_book_genre('Добро пожаловать', 'Комедии')
+        collector.set_book_genre('Анна Каренина', 'Роман')
+        assert collector.get_books_for_children() == ['Добро пожаловать']
+
+    def test_add_book_in_favorites(collector, book, expected_favorites):
+        collector = BooksCollector()
+        collector.set_book_genre('Отель', 'Детективы')
+        collector.set_book_genre('Пуаро', 'Детективы')
+        collector.add_book_in_favorites(book)
+
+        assert collector.get_list_of_favorites_books() == expected_favorites
+
+    def test_delete_book_from_favorites(collector, book, expected_favorites):
+        collector = BooksCollector()
+        collector.set_book_genre('Отель', 'Детективы')
+        collector.set_book_genre('Пуаро', 'Детективы')
+        collector.add_book_in_favorites('Отель')
+        collector.add_book_in_favorites('Пуаро')
+        collector.delete_book_from_favorites(book)
+
+        assert collector.get_list_of_favorites_books() == expected_favorites
+
+    def test_get_list_of_favorites_books(collector, book, expected_favorites):
+        collector = BooksCollector()
+        collector.collector.add_new_book('Крик')
+        collector.collector.add_book_in_favorites('Крик')
+        favorites = collector.get_list_of_favorites_books()
+        assert len(favorites) == 1
+        assert (favorites[0] == 'Крик')
